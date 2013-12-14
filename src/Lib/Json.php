b47bc632ad5f239c1f50a49b8579b6d77c7323fa
Alexander Wühr <wuehr@sc-networks.com>
2013-12-14 19:44:58 +0100

    Minor cosmetic changes

--- src/Lib/Json.php
+++ src/Lib/Json.php
@@ -2,13 +2,23 @@
 
 namespace Lx\JsonRpcXp\Lib;
 
-use Composer\Json\JsonValidationException;
-
 class Json {
+
+	/**
+	 * @param mixed $data
+	 *
+	 * @return string
+	 */
 	public static function encode($data) {
 		return json_encode($data);
 	}
 
+	/**
+	 * @param string $json_string
+	 *
+	 * @return mixed
+	 * @throws \InvalidArgumentException
+	 */
 	public static function decode($json_string) {
 		if (!$data = json_decode($json_string, true)) {
 			throw new \InvalidArgumentException('Could not parse json string');
